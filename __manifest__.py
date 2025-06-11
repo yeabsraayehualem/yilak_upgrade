96f9b5763d690850f0ca46c26671133972410de7
Solomon-Asefa <soloasefa6603@gmail.com>
2025-06-11 12:09:10 +0300

    Initial module setup for custom_sale_approval_void

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Custom Sale Approval and Voiding',
+    'version': '1.0',
+    'category': 'Sales',
+    'summary': 'Add approval and voiding features to Sales module in Odoo 17',
+    'depends': ['sale_management'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
